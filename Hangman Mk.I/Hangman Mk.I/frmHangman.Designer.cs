f99146d4e0a5973431627476d41b593ddcfd924c
joshwalls <josh@wallsfamily.co.uk>
2014-01-26 15:41:21 +0000

    0.0.2r2 added multiple languages

--- Hangman Mk.I/Hangman Mk.I/frmHangman.Designer.cs
+++ Hangman Mk.I/Hangman Mk.I/frmHangman.Designer.cs
@@ -28,40 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageLanguage = new System.Windows.Forms.TabPage();
+            this.tabPageHangman = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxLanguage = new System.Windows.Forms.ComboBox();
+            this.btnLanguageGo = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPageLanguage.SuspendLayout();
+            this.tabPageHangman.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageHangman);
+            this.tabControl1.Controls.Add(this.tabPageLanguage);
+            this.tabControl1.Location = new System.Drawing.Point(12, 49);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(359, 457);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageLanguage
+            // 
+            this.tabPageLanguage.Controls.Add(this.textBox1);
+            this.tabPageLanguage.Location = new System.Drawing.Point(4, 22);
+            this.tabPageLanguage.Name = "tabPageLanguage";
+            this.tabPageLanguage.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPageLanguage.Size = new System.Drawing.Size(351, 431);
+            this.tabPageLanguage.TabIndex = 0;
+            this.tabPageLanguage.Text = "Hangman";
+            this.tabPageLanguage.UseVisualStyleBackColor = true;
+            // 
+            // tabPageHangman
+            // 
+            this.tabPageHangman.Controls.Add(this.btnLanguageGo);
+            this.tabPageHangman.Controls.Add(this.comboBoxLanguage);
+            this.tabPageHangman.Controls.Add(this.label2);
+            this.tabPageHangman.Location = new System.Drawing.Point(4, 22);
+            this.tabPageHangman.Name = "tabPageHangman";
+            this.tabPageHangman.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPageHangman.Size = new System.Drawing.Size(351, 431);
+            this.tabPageHangman.TabIndex = 1;
+            this.tabPageHangman.Text = "Language";
+            this.tabPageHangman.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hangman";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(7, 7);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(200, 31);
+            this.textBox1.Size = new System.Drawing.Size(338, 31);
             this.textBox1.TabIndex = 0;
             // 
-            // pictureBox1
+            // label2
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.Image = global::Hangman_Mk.I.Properties.Resources.hang0;
-            this.pictureBox1.ImageLocation = "";
-            this.pictureBox1.Location = new System.Drawing.Point(13, 51);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 131);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Select Language";
+            // 
+            // comboBoxLanguage
+            // 
+            this.comboBoxLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxLanguage.FormattingEnabled = true;
+            this.comboBoxLanguage.Items.AddRange(new object[] {
+            "Afrikaans",
+            "Belarusian",
+            "Bulgarian",
+            "Catalan",
+            "Croatian",
+            "Czech",
+            "Danish",
+            "English",
+            "English (US)",
+            "French",
+            "German",
+            "Greek",
+            "Hungarian",
+            "Italian",
+            "Latin",
+            "Mongolian",
+            "Polish",
+            "Portuguese",
+            "Romanian",
+            "Russian",
+            "Serbian",
+            "Slovak",
+            "Slovenian",
+            "Spanish",
+            "Swedish",
+            "Ukrainian"});
+            this.comboBoxLanguage.Location = new System.Drawing.Point(7, 31);
+            this.comboBoxLanguage.Name = "comboBoxLanguage";
+            this.comboBoxLanguage.Size = new System.Drawing.Size(338, 32);
+            this.comboBoxLanguage.Sorted = true;
+            this.comboBoxLanguage.TabIndex = 1;
+            // 
+            // btnLanguageGo
+            // 
+            this.btnLanguageGo.Location = new System.Drawing.Point(270, 70);
+            this.btnLanguageGo.Name = "btnLanguageGo";
+            this.btnLanguageGo.Size = new System.Drawing.Size(75, 23);
+            this.btnLanguageGo.TabIndex = 2;
+            this.btnLanguageGo.Text = "Go";
+            this.btnLanguageGo.UseVisualStyleBackColor = true;
+            this.btnLanguageGo.Click += new System.EventHandler(this.btnLanguageGo_Click);
             // 
             // frmHangman
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 497);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(384, 521);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
             this.Name = "frmHangman";
             this.Text = "Hangman";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageLanguage.ResumeLayout(false);
+            this.tabPageLanguage.PerformLayout();
+            this.tabPageHangman.ResumeLayout(false);
+            this.tabPageHangman.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,8 +173,15 @@
 
         #endregion
 
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageHangman;
+        private System.Windows.Forms.Button btnLanguageGo;
+        private System.Windows.Forms.ComboBox comboBoxLanguage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPageLanguage;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+
     }
 }
 
--- Hangman Mk.I/Hangman Mk.I/frmHangman.cs
+++ Hangman Mk.I/Hangman Mk.I/frmHangman.cs
@@ -17,22 +17,167 @@ namespace Hangman_Mk.I
         {
             InitializeComponent();
 
+            comboBoxLanguage.SelectedItem = "English";
 
-            StreamReader reader = new StreamReader("Resources/dictionary.txt");
+            //string[] words = File.ReadAllLines("Resources/dictionary.txt");
 
-            string fileString = "";
-            int indexInteger = 0;
+            //string[] words = new string[] { "hello", "goodbye" };
 
-            string[] words = new string[] { };
+            //int wordsLength = words.Length;
 
-            while (fileString != null)
+            
+
+            
+        }
+
+        int wordsLength;
+
+        private void btnLanguageGo_Click(object sender, EventArgs e)
+        {
+            if (comboBoxLanguage.Text == "Afrikaans")
             {
-                fileString = reader.ReadLine();
-                if (fileString != null)
-                {
-                    words[indexInteger] = fileString;
-                }
+                string[] words = File.ReadAllLines("Resources/afrikaans.txt");
+                wordsLength = words.Length;
             }
+            else if (comboBoxLanguage.Text == "Belarusian")
+            {
+                string[] words = File.ReadAllLines("Resources/belarusian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Bulgarian")
+            {
+                string[] words = File.ReadAllLines("Resources/bulgarian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Catalan")
+            {
+                string[] words = File.ReadAllLines("Resources/catalan.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Croatian")
+            {
+                string[] words = File.ReadAllLines("Resources/croatian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Czech")
+            {
+                string[] words = File.ReadAllLines("Resources/czech.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Danish")
+            {
+                string[] words = File.ReadAllLines("Resources/danish.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Dutch")
+            {
+                string[] words = File.ReadAllLines("Resources/dutch.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "English")
+            {
+                string[] words = File.ReadAllLines("Resources/english.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "English (US)")
+            {
+                string[] words = File.ReadAllLines("Resources/american.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "French")
+            {
+                string[] words = File.ReadAllLines("Resources/french.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "German")
+            {
+                string[] words = File.ReadAllLines("Resources/german.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Greek")
+            {
+                string[] words = File.ReadAllLines("Resources/greek.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Hungarian")
+            {
+                string[] words = File.ReadAllLines("Resources/hungarian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Italian")
+            {
+                string[] words = File.ReadAllLines("Resources/italian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Latin")
+            {
+                string[] words = File.ReadAllLines("Resources/latin.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Mongolian")
+            {
+                string[] words = File.ReadAllLines("Resources/mongolian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Polish")
+            {
+                string[] words = File.ReadAllLines("Resources/polish.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Portuguese")
+            {
+                string[] words = File.ReadAllLines("Resources/portuguese.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Romanian")
+            {
+                string[] words = File.ReadAllLines("Resources/romanian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Russian")
+            {
+                string[] words = File.ReadAllLines("Resources/russian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Serbian")
+            {
+                string[] words = File.ReadAllLines("Resources/serbian.txt");
+                wordsLength = words.Length;
+
+            }
+            else if (comboBoxLanguage.Text == "Slovak")
+            {
+                string[] words = File.ReadAllLines("Resources/slovak.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Slovenian")
+            {
+                string[] words = File.ReadAllLines("Resources/slovianian.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Spanish")
+            {
+                string[] words = File.ReadAllLines("Resources/spanish.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Swedish")
+            {
+                string[] words = File.ReadAllLines("Resources/swedish.txt");
+                wordsLength = words.Length;
+            }
+            else if (comboBoxLanguage.Text == "Ukrainian")
+            {
+                string[] words = File.ReadAllLines("Resources/ukranian.txt");
+                wordsLength = words.Length;
+            }
+            else
+            {
+                MessageBox.Show("Sorry, we don't have that language" + "\n" + "Select one from the list");
+            }
+
+            Console.WriteLine(wordsLength);
+            //Console.ReadLine();
+            
         }
     }
 }
